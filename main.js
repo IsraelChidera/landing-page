7a6dd8115aec9ceeb820e5e411b3700bd60d2bb3
israelchidera <israelchidera54@gmail.com>
2020-08-05 22:43:20 +0100

    first commit without mobile hamburger menu

---
+++ main.js
@@ -0,0 +1,10 @@
+function openSideMenu() {
+    document.querySelector('#sideMenu').style.width = '250px';
+    // document.getElementById('main').style.margin = '150px';
+}
+
+function closeSideMenu() {
+    document.querySelector('#sideMenu').style.width = '0';
+    // document.getElementById('main').style.margin = 'auto';
+}
+
